cd5c21558f7dc0aa6843cf5c919788c16e3228e7
Marko Miskovic <misko23@gmail.com>
2025-05-14 00:01:30 +0200

    Using Axios with CookieJar to avoid Puppeteer. Final Docker size is considerably smaller.

--- src/checkAppointments.js
+++ src/checkAppointments.js
@@ -1,29 +1,22 @@
-const fs = require('fs');
-const puppeteer = require('puppeteer');
+const axios = require('axios').default;
+const { wrapper } = require('axios-cookiejar-support');
+const { CookieJar } = require('tough-cookie');
 const {sendNotification} = require("./notifications");
 const {logWithTimestamp} = require("./utils");
 
 async function checkAppointment() {
-    const options = {
-        headless: true,
-        args: ['--disable-gpu', '--no-sandbox', '--disable-dev-shm-usage']
-    };
-    // If there is a local Chromium installed, we'll be using that one.
-    if (fs.existsSync('/usr/bin/chromium')) {
-        options.executablePath = '/usr/bin/chromium'; // Your own lightweight Chromium
-    }
-    const browser = await puppeteer.launch(options);
-    const page = await browser.newPage();
+    const jar = new CookieJar();
+    const client = wrapper(axios.create({ jar }));
 
     try {
         const urlPageA = 'https://service.berlin.de/dienstleistung/351180/';
-        await page.goto(urlPageA, {waitUntil: 'networkidle2'});
-
         const urlPageB = 'https://service.berlin.de/terminvereinbarung/termin/all/351180/';
-        const responsePageB = await page.goto(urlPageB, {waitUntil: 'networkidle2'});
-        const contentPageB = await page.content();
 
-        if (responsePageB.status() === 403) {
+        await client.get(urlPageA);
+        const responsePageB = await client.get(urlPageB);
+        const contentPageB = responsePageB.data;
+
+        if (responsePageB.status === 403) {
             logWithTimestamp('❌ Forbidden access (Suspicious network, malware scanning, intrusion attempt, ...).');
         } else if (contentPageB.includes('Leider sind aktuell keine Termine für ihre Auswahl verfügbar')) {
             logWithTimestamp('❌ No appointment available');
@@ -34,8 +27,6 @@ async function checkAppointment() {
         }
     } catch (err) {
         console.error('Error checking the page:', err);
-    } finally {
-        await browser.close();
     }
 }
 
